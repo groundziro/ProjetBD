2d632e3bcdc38b993607d3f3583e35309d469c46
Alfatta <alfatta_@hotmail.com>
2017-11-28 15:55:31 +0100

    DebutNetbeans
    a

---
+++ OnNetbeans/ProjetBD/src/projetbd/SecondeClasse.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetbd;
+
+/**
+ *
+ * @author Alfa
+ */
+public class SecondeClasse {
+    static void yo(){
+        System.out.println("bob");
+    }
+}
