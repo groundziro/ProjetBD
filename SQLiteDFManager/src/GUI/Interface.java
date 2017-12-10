32cd88d5bfe7d13e510e4ee30f6e141252eb3ff1
groundziro <33598078+groundziro@users.noreply.github.com>
2017-12-10 15:03:02 +0100

    Gui

---
+++ SQLiteDFManager/src/GUI/Interface.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.File;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Interface extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button Browse = new Button("Browse");
+        Browse.setOnAction(actionEvent->{
+            FileChooser choose = new FileChooser();
+            choose.getExtensionFilters().add(new ExtensionFilter("DataBases","*.db"));
+            File result = choose.showOpenDialog(primaryStage);
+        });
+        BorderPane root = new BorderPane();
+        Region up = new Region();
+        up.setPrefSize(100,50);
+        root.setCenter(Browse);
+        root.setTop(up);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+}
