b572a7dce2deb2ea9832992cbbc50f8fe5177683
groundziro <33598078+groundziro@users.noreply.github.com>
2017-12-10 22:59:59 +0100

    Rajout de Boutons
    j'ai rajouté les boutons modify, qui va modifier une df, delete, qui supprime une DF que l'on peut choisir, Check, qui va checker une table si elle est en BCNF ou en 3NF (checkBCNF() et check3NF() à implémenter).
    Je peux encore rajouter un bouton qui va vérifier les conséquences logiques (pour les afficher sous forme de boutons pour en supprimer si l'on souhaite. Il manque juste le check entre 2 DFs pour savoir si l'une est une conséquence logique de l'autre).
    Il manquera ainsi les clés et superclés et ce sera à peu près fini. Si jamais il y a un truc que tu ne comprends pas dis-le moi direct.

--- SQLiteDFManager/src/GUI/Interface.java
+++ SQLiteDFManager/src/GUI/Interface.java
@@ -9,6 +9,9 @@ package GUI;
 import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,7 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
@@ -34,6 +37,7 @@ public class Interface extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button Browse = new Button("Browse");
+        BorderPane choice = new BorderPane();
         BorderPane root = new BorderPane();
         Region up = new Region();
         up.setPrefSize(100,50);
@@ -41,33 +45,85 @@ public class Interface extends Application {
         root.setTop(up);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        Browse.setOnAction(actionEvent->{
-            FileChooser choose = new FileChooser();
-            choose.getExtensionFilters().add(new ExtensionFilter("DataBases","*.db"));
-            File result = choose.showOpenDialog(primaryStage);
-            BorderPane p = new BorderPane();
-            DFManager dfs = null;
-            try{
-                dfs = new DFManager(result.getAbsolutePath());
-            }catch(SQLException e){
-                System.out.println(e.getMessage());
+        Browse.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                FileChooser choose = new FileChooser();
+                choose.getExtensionFilters().add(new ExtensionFilter("DataBases","*.db"));
+                File result = choose.showOpenDialog(primaryStage);
+                BorderPane p = new BorderPane();
+                DFManager dfs = null;
+                try{
+                    dfs = new DFManager(result.getAbsolutePath());
+                }catch(SQLException e){
+                    System.out.println(e.getMessage());
+                }
+                List<Button> btns = new ArrayList<>();
+                List<Button> dfBtns = new ArrayList<>();
+                try{
+                    for(DF df : dfs.getDFs()){
+                        btns.add(new Button(df.toString()));
+                    }
+                }catch(SQLException ex){
+                    System.out.println(ex.getMessage());
+                }
+                try{
+                    for(String table : dfs.getTabNames()){
+                        btns.add(new Button(table));
+                    }
+                }catch(SQLException ex){
+                    System.out.println(ex.getMessage());
+                }
+                Button Add = new Button("Add DF");
+                Button Exit = new Button("Exit");
+                Button Check = new Button("Check...");
+                Button Modify = new Button("Modify...");
+                Button Delete = new Button("Delete...");
+                Delete.setOnAction((ActionEvent del)->{
+                    VBox v = new VBox();
+                    for(Button b : dfBtns){
+                        b.setOnAction(del1->{
+                            dfs.getDFs().remove(df);
+                        });
+                        v.getChildren().add(b);
+                    }
+                   choice.setCenter(v);
+                   primaryStage.setScene(new Scene(choice));
+                });
+                Modify.setOnAction(mod->{
+                    VBox v = new VBox();
+                    for(Button b : dfBtns){
+                        v.getChildren().add(b);
+                    }
+                   choice.setCenter(v);
+                });
+                Exit.setOnAction(quit->{
+                    primaryStage.setScene(scene);
+                });
+                p.setBottom(new HBox(Add,Exit,Check,Modify,Delete));
+                try{
+                    p.setCenter(current(dfs));
+                }catch(SQLException e){
+                    System.out.println(e.getMessage());
+                }
+                Check.setOnAction(check->{
+                    VBox v = new VBox();
+                    for(Button b : btns){
+                        b.setOnAction(check1->{
+                            checkBCNF(b.getText());
+                            check3NF(b.getText());
+                        });
+                        v.getChildren().add(b);
+                    }
+                   choice.setCenter(v);
+                   primaryStage.setScene(new Scene(choice));
+                });
+                Scene Tables = new Scene(p);
+                primaryStage.setScene(Tables);
             }
-            Button Add = new Button("Add DF");
-            Button Exit = new Button("Exit");
-            Exit.setOnAction(quit->{
-                primaryStage.setScene(scene);
-            });
-            p.setBottom(new HBox(Add,Exit));
-            try{
-                p.setCenter(current(dfs));
-            }catch(SQLException e){
-                System.out.println(e.getMessage());
-            }
-            Scene Tables = new Scene(p);
-            primaryStage.setScene(Tables);
         });
         primaryStage.show();
-    }
+        }
     
     /*private Text current(DFManager df) throws SQLException{
         Text txt = new Text();
@@ -94,7 +150,18 @@ public class Interface extends Application {
         txt.setText(str);
         return txt;
     }
-    
+    private void initVBox(VBox v, DFManager dfs)throws SQLException{
+        for(DF df : dfs.getDFs()){
+            Button b = new Button(df.toString());
+            v.getChildren().add(b);
+        }
+    }
+    private boolean check3NF(String table){
+        return DF.check3NF(table);
+    } 
+    private boolean checkBCNF(String table){
+        return DF.checkBCNF(table);
+    }
     
     /**
      * @param args the command line arguments
