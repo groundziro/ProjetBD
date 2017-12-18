120ecec4da41d125b458267c83ed5a66766fb201
groundziro <33598078+groundziro@users.noreply.github.com>
2017-12-18 15:57:33 +0100

    Réparation

---
+++ SQLiteDFManager/src/main/Key.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alfa
+ */
+public class Key {
+    ArrayList<String> attributes;
+
+    public Key() {
+        attributes=new ArrayList();
+    }
+
+    public Key(ArrayList<String> attributes) {
+        this.attributes = attributes;
+    }
+
+    public void addAttributes(String newAtr){
+        attributes.add(newAtr);
+    }
+}
